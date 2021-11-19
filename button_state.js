a0bd6f68ac35c42d29f022847c18b6572fec09be
Hayden <haydendonald@gmail.com>
2021-11-19 12:51:40 +1100

    Fixed a bug with multiple nodes breaking styling

--- button_state.js
+++ button_state.js
@@ -17,43 +17,39 @@ module.exports = function (RED) {
     //Generate the HTML
     var generateHTML = function (config) {
         var height = config.height != 0 ? config.height : 20;
-        var HTML = "";
+        var HTML = "<div>";
 
         //Add the CSS
-        HTML += String.raw`
-        <style>
-            .container {
-                padding: 0;
-                margin: 0;
-                margin-left: 2.5px;
-                margin-right: 2.5px;
-                float: left;
-                display: inline-block;
-                width: calc(calc(100% / ${Math.ceil(config.options.length / height)}) - 5px);
-            }
-            .optionButton {
-                width: 100%;
-                height: 100%;
-            }
-        </style>
-        <div>
+        var containerCSS = String.raw`
+            padding: 0;
+            margin: 0;
+            margin-left: 2.5px;
+            margin-right: 2.5px;
+            float: left;
+            display: inline-block;
+            width: calc(calc(100% / ${Math.ceil(config.options.length / height)}) - 5px);
+        `
+
+        var optionButtonCSS = String.raw`
+            width: 100%;
+            height: 100%;
         `;
 
         //Add a button to the HTML
         var addButton = function (value) {
             HTML += String.raw`
-                <md-button class='optionButton' style="background-color: {{(value == '${value.value}') ? '${value.onColor}': '${value.offColor}'}} !important" ng-click="buttonClicked('${value.value}')">${value.label}</md-button>
+                <md-button style="${optionButtonCSS} background-color: {{(value == '${value.value}') ? '${value.onColor}': '${value.offColor}'}} !important" ng-click="buttonClicked('${value.value}')">${value.label}</md-button>
             `;
         }
 
-        HTML += "<div class='container'>";
+        HTML += String.raw`<div style="${containerCSS}">`;
 
         //Add the buttons for the values
         var j = 0;
         for (var i = 0; i < config.options.length; i++) {
             //If we go outside our height bounds move over to the next column
             if (j >= parseInt(height)) {
-                HTML += "</div><div class='container'>";
+                HTML += String.raw`</div><div style="${containerCSS}">`;
                 j = 0;
             }
             addButton(config.options[i]);

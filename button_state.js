7dae6f23d668fa356fe867de8fd75efe13061e4c
Hayden <haydendonald@gmail.com>
2021-11-22 16:56:47 +1100

    Fixed styling

--- button_state.js
+++ button_state.js
@@ -15,28 +15,10 @@
  **/
 module.exports = function (RED) {
     //Generate the HTML
-    var generateHTML = function (config, width, height) {
-        var numButtonsY = parseInt(config.height != 0 ? config.height : 20);
-        var buttonHeight = (height / numButtonsY) - 10;
-
-        var HTML = "<div>";
-        
-        //Add the CSS
-        var containerCSS = String.raw`
-            padding: 0;
-            margin: 0;
-            margin-left: 2.5px;
-            margin-right: 2.5px;
-            float: left;
-            display: inline-block;
-            width: ${(width / Math.ceil(config.options.length / numButtonsY)) - 5}px;
-            height: ${height - 5}px;
-            background-color: green;
-        `
+    var generateHTML = function (config) {
+        var HTML = String.raw`<div style="width: 100%; height: 100%; opacity: 0; display: none; transition: opacity 0.5s;">`;
         var optionButtonCSS = String.raw`
             width: 100%;
-            min-height: ${buttonHeight}px;
-            max-height: ${buttonHeight}px;
             padding: 0;
             margin: 0;
             margin-top: 2.5px;
@@ -46,18 +28,18 @@ module.exports = function (RED) {
         //Add a button to the HTML
         var addButton = function (value) {
             HTML += String.raw`
-                <md-button style="${optionButtonCSS} background-color: {{(value == '${value.value}') ? '${value.onColor}': '${value.offColor}'}} !important" ng-click="buttonClicked('${value.value}')">${value.label}</md-button>
+                <md-button style="${optionButtonCSS} background-color: ${value.offColor}" value="${value.value}" oncolor="${value.onColor}" offcolor="${value.offColor}" ng-click="buttonClicked('${value.value}')">${value.label}</md-button>
             `;
         }
 
-        HTML += String.raw`<div style="${containerCSS}">`;
+        HTML += String.raw`<div>`;
 
         //Add the buttons for the values
         var j = 0;
         for (var i = 0; i < config.options.length; i++) {
             //If we go outside our height bounds move over to the next column
-            if (j >= numButtonsY) {
-                HTML += String.raw`</div><div style="${containerCSS}">`;
+            if (j >= parseInt(config.height)) {
+                HTML += String.raw`</div><div>`;
                 j = 0;
             }
             addButton(config.options[i]);
@@ -75,16 +57,16 @@ module.exports = function (RED) {
             if (ui === undefined) {
                 ui = RED.require("node-red-dashboard")(RED);
             }
-            
+
             RED.nodes.createNode(this, config);
             var done = ui.addWidget({
                 node: node,
-                format: generateHTML(config, (ui.getSizes().sx * config.width) + 18, ui.getSizes().sy * config.height + 21.25), //The 18 and 21.25 values are the difference in the value and what is actually on the dashboard
+                format: generateHTML(config),
                 templateScope: "local",
                 group: config.group,
                 emitOnlyNewValues: false,
                 forwardInputMessages: false,
-                storeFrontEndInputAsState: false,
+                storeFrontEndInputAsState: true,
                 group: config.group,
                 order: config.order,
                 width: config.width,
@@ -102,17 +84,80 @@ module.exports = function (RED) {
 
                 //Setup the angular parameters
                 initController: function ($scope, events) {
-                    //Set our width (supporting old browsers that don't support calc)
                     $scope.value = "";
+                    $scope.buttons = {};
+
+                    //Find the containing element
+                    setTimeout(function () {
+                        for (var i = 0; i < document.getElementsByTagName("md-card").length; i++) {
+                            var curr = document.getElementsByTagName("md-card")[i];
+                            if (curr.getAttribute("node-id") == id) {
+                                var width = curr.clientWidth;
+                                var height = curr.clientHeight;
+                                curr.style.padding = 0;
+                                curr.style.margin = 0;
+
+                                var containerWidth = (width / (curr.getElementsByTagName("div").length - 1)) - 5;
+                                var buttonHeight = (height / $scope.height) - 5;
+
+                                //Set our size based on the rendered size
+                                for (var j = 1; j < curr.getElementsByTagName("div").length; j++) {
+                                    var currDiv = curr.getElementsByTagName("div")[j];
+                                    var containerCSS = String.raw`
+                                    padding: 0;
+                                    margin: 0;
+                                    margin-left: 2.5px;
+                                    margin-right: 2.5px;
+                                    float: left;
+                                    width: ${containerWidth}px;
+                                    height: 100%;
+                                    display: inline-block;
+                                `;
+                                    currDiv.style = containerCSS;
+                                    for (var k = 0; k < currDiv.getElementsByTagName("button").length; k++) {
+                                        var currButton = currDiv.getElementsByTagName("button")[k];
+                                        currButton.style.height = String.raw`${buttonHeight}px`;
+                                        if ($scope.buttons[currButton.getAttribute("value")] === undefined) { $scope.buttons[currButton.getAttribute("value")] = []; }
+                                        $scope.buttons[currButton.getAttribute("value")].push(currButton);
+                                    }
+                                }
+                                $scope.updateButtons($scope.value);
+
+                                //Show the element with a nice fade once everything has loaded since this will happen after the page is loaded :(
+                                curr.getElementsByTagName("div")[0].style.display = "block";
+                                setTimeout(function () { curr.getElementsByTagName("div")[0].style.opacity = 1; }, 100);
+                            }
+                        }
+
+                    }, 300); //The timeout exists to allow the DOM to render in before we grab it's values to do our calculations
+
 
                     //When a button is clicked
                     $scope.buttonClicked = function (value) {
-                        $scope.send({ payload: value });
+                        $scope.send([{ payload: value }, undefined]);
+                    }
+
+                    $scope.updateButtons = function (newValue) {
+                        //Update our buttons
+                        try {
+                            for (var j = 0; j < $scope.buttons[$scope.value].length; j++) {
+                                $scope.buttons[$scope.value][j].style.backgroundColor = $scope.buttons[$scope.value][j].getAttribute("offColor");
+                            }
+                        }
+                        catch (e) { }
+                        try {
+                            for (var j = 0; j < $scope.buttons[newValue].length; j++) {
+                                $scope.buttons[newValue][j].style.backgroundColor = $scope.buttons[newValue][j].getAttribute("onColor");
+                            }
+                        }
+                        catch (e) { }
                     }
 
                     //When a message comes on the input and is sent from before emit
                     $scope.$watch("msg", function (msg) {
                         if (!msg) { return; }
+
+                        $scope.updateButtons(msg.payload);
                         $scope.value = msg.payload;
                     });
                 },
@@ -121,7 +166,6 @@ module.exports = function (RED) {
                 beforeEmit: function (msg, value) {
                     return { msg };
                 }
-
             });
 
         }
